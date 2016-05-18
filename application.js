27b91b097dac6cf20a366538a6f95e26b09380f2
shingtzewoo@gmail.com <shingtzewoo@gmail.com>
2016-05-17 18:54:54 -0700

    fixed the changes

--- application.js
+++ application.js
@@ -1,4 +1,3 @@
-
 //the default option for building the sketch pad when the page first loads;
 var buildSquares = function () {
   var count = 256;
@@ -8,18 +7,17 @@ var buildSquares = function () {
 }
 //when mouse hovers over squares, changes their color into a random color;
 var randomColor = function () {
-  $("#container").on('mouseenter', 'div', function() {
-    event.preventDefault();
+  $('#container > div').mouseenter(function() {
     $(this).css('background-color', "#"+((1<<24)*Math.random()|0).toString(16));
-    });
+  });
 }
 //when mouse hovers over squares, changes their color into black;
 var blackColor = function () {
   $("#container").on('mouseenter', 'div', function() {
-    event.preventDefault();
     $(this).addClass('black');
   });
 }
+//this function prompts the user how many squares they would like for their grid
 var buildSquares2 = function () {
       var ask = parseInt(prompt("How many squares per side would you want for your new sketch grid? (Max = 64)", "16"));
       var value = ask * ask;
@@ -42,26 +40,27 @@ var buildSquares2 = function () {
       }
       else {
         alert("Please enter a number between 1 and 64");
+        var ask = parseInt(prompt("How many squares per side would you want for your new sketch grid? (Max = 64)", "16"));
       }
     };
 
 $(document).ready(function() {
+  // the default, 16 x 16 grid, squares will be colored black when mouse enters
   buildSquares();
   blackColor();
+  // When the #clear button is clicked, a new sketch grid can be generated and when the mouse enters, the color black will be generated for the background-color of that particular square.
   $("#clear").on('click', function() {
     event.preventDefault();
-    event.stopPropagation();
-    $("div").removeClass('black');
     $('div > div').remove();
     buildSquares2();
     blackColor();
-  });
-  // When #random button is clicked, a new sketch grid can be generated and
+  })
+// When #random button is clicked, a new sketch grid can be generated and when the mouse enters, random colors will be generated for the background-color of that particular square.
   $("#random").on('click', function() {
     event.preventDefault();
     $('div > div').remove();
     buildSquares2();
     randomColor();
-  });
+  })
 });
 
